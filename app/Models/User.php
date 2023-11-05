07a5daf4c69690a61dd3a42270c4198d284e5da0
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-11-05 11:32:54 +0900

    コメント投稿作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(post::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
